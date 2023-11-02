59d2d07845cced10cc80f90da0c78028225faee1
Gofiix4 <carlos.eht.09@gmail.com>
2023-11-02 16:54:23 -0600

    debug false

--- APITESCHI/settings.py
+++ APITESCHI/settings.py
@@ -28,7 +28,7 @@ ALLOWED_HOSTS = ["*"]
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-DEBUG = True
+DEBUG = False
 
 """
 DEBUG = 'RENDER' not in os.environ
