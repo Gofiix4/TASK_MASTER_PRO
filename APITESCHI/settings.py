dcaadea9605a5fc7e57c33d71bd8b916b0ee0e4b
Gofiix4 <carlos.eht.09@gmail.com>
2023-11-11 10:31:50 -0600

    hostname

--- APITESCHI/settings.py
+++ APITESCHI/settings.py
@@ -35,7 +35,7 @@ if not DEBUG:
     # in your application directory on Render.
     STATIC_ROOT = BASE_DIR / 'api/static'
     
-RENDER_EXTERNAL_HOSTNAME = False
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
