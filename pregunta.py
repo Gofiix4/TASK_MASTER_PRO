31c7457da8ed7519b0417890e90ac0c1f198500d
Gofiix4 <carlos.eht.09@gmail.com>
2023-12-15 13:11:45 -0600

    despues de otra tragedia

--- pregunta.py
+++
@@ -1,8 +0,0 @@
-import openai
-
-openai.api_key = "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"
-pregunta = openai.Completion.create(engine="text-davinci-003",
-                                    prompt="¿Que es chatgpt?",
-                                    max_tokens=2048)
-
-print(pregunta.choices[0].text)
